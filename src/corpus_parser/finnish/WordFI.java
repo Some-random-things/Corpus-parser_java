8614671ee0715730778d06caac58c35f5b542dc1
amorphousxd <amorphousxd@gmail.com>
2013-11-23 00:55:29 +0400

    Added WordFI

---
+++ src/corpus_parser/finnish/WordFI.java
@@ -0,0 +1,30 @@
+package corpus_parser.finnish;
+
+import corpus_parser.Word;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Pavel
+ * Date: 22.11.13
+ * Time: 23:00
+ * To change this template use File | Settings | File Templates.
+ */
+public class WordFI implements Word {
+    public int id;
+    public int dom; //dom means value of <dep gov= ...> attribute
+    public String feat; //feat means <token>   <posreading rawtags= ...> attribute
+    public String lemma; //feat means <token> <posreading baseform= ...> attribute
+    public String[] featValues;
+    public String link;
+
+
+    public WordFI(int _dom, String _feat, int _id, String _lemma, String _link)
+    {
+        this.dom = _dom;
+        this.feat = _feat;
+        this.id = _id;
+        this.lemma = _lemma;
+        this.link = _link;
+        this.featValues = this.feat.split(" ");
+    }
+}
