c347f2998916695a7ba55bfdb99d4c4b2f1c49c0
amorphousxd <amorphousxd@gmail.com>
2013-11-26 17:12:55 +0400

    Fixed previous changes

--- src/corpus_parser/Main.java
+++ src/corpus_parser/Main.java
@@ -19,53 +19,24 @@ import java.util.Map;
  */
 public class Main {
 
+    public static String language;
+    public static String resultsPath;
     public static HashMap<String, Integer> stats = new HashMap<String, Integer>();
-
-    public static void setStats(final File folder, String language) {
-        for (final File fileEntry : folder.listFiles()) {
-            if (fileEntry.isDirectory()) {
-                setStats(fileEntry, language);
-            } else {
-                System.out.println(fileEntry.getAbsolutePath());
-                if(language == "finnish"){
-                    ParserFI p = new ParserFI(fileEntry.getAbsolutePath());
-                    p.getStats();
-                }
-                if(language == "russian"){
-                    ParserRU p = new ParserRU(fileEntry.getAbsolutePath());
-                    p.getStats();
-                }
-            }
-        }
-    }
-
 
     public static void main(String[] args) {
 
         final File folderFI = new File("C:\\corpus_fin");
-        String language = "finnish";
-        setStats(folderFI, language);
+        resultsPath = "C:\\corpus_fi\\results_fin.txt";
+        language = "finnish";
+        StatsManagement.getStats(folderFI, language);
 
         /*final File folderRU = new File("C:\\corpus");
-        String language = "russian";
-        setStats(folderRU, language); */
+        resultsPath = "C:\\corpus_stats\\results_ru.txt";
+        language = "russian";
+        StatsManagement.getStats(folderRU, language); */
 
-        try {
-            FileWriter fw = new FileWriter("C:\\corpus_fi\\results.txt");
-            BufferedWriter out = new BufferedWriter(fw);
+        StatsManagement.writeStats(resultsPath);
 
-            Iterator<Map.Entry<String, Integer>> it = stats.entrySet().iterator();
-            while (it.hasNext()) {
-                Map.Entry<String, Integer> pairs = it.next();
-                out.write(pairs.getKey() + " ; " + pairs.getValue());
-                out.newLine();
-            }
-
-            out.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
 
         System.out.println(stats.get("A>V"));
